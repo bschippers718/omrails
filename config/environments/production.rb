ab36eaefc9c5789838fb03675c9f1b34bd711742
bschippers718 <bschippers718@gmail.com>
2013-08-19 18:57:46 -0400

    fuck youuuu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Omrails::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
+  # Disable Rails's static asset server (Apache or nginx will werwalready do this).
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
